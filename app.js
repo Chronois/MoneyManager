f391af8e7a382a9715dd1b7b0c2e224535e35d2f
Chronois <105849434+Chronois@users.noreply.github.com>
2026-07-23 10:33:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1028,7 +1028,7 @@ function renderTransactions(){
   });
 
   el.querySelectorAll('[data-edit]').forEach(b=> b.addEventListener('click', ()=> openTxnModal(Number(b.dataset.edit))));
-  el.querySelectorAll('[data-dup]').forEach(b=> b.addEventListener('click', ()=> openTxnModal(Number(b.dataset.dup), true)));
+  el.querySelectorAll('[data-dup]').forEach(b=> b.addEventListener('click', ()=> openTxnModal(Number(b.dataset.dup), true))));
   el.querySelectorAll('[data-del]').forEach(b=> b.addEventListener('click', ()=> deleteTxn(Number(b.dataset.del))));
 }
 
